a272058301ce0e01e2ecaf0ffa64b2b675f3f477
Ats-Naka <nakamura@naganogiken.co.jp>
2023-06-16 11:17:30 +0900

    useRef,Optional Chaining,Non-Null-Assertion Operator

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -17,12 +17,24 @@ const Counter: React.FC<{}> = () => {
     renderTimes.current = renderTimes.current + 1;
   });
 
+  const ref=useRef<HTMLInputElement>(null!);  //!をつけるとnullであることを否定する
+  const focusInput = () => {
+    /*
+    const current = ref.current;
+    if (current!=null) current.focus();
+    */
+    //ref.current?.focus(); //currentがnullだったらfocus()は実行されない方法
+    ref.current.focus();
+  }
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 }
